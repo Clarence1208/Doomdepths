dd4cc017bb7f216986228e0e0058504edc3d83b6
Moutoo <ninoplane@gmail.com>
2023-10-20 13:41:32 +0200

    Wip on map

--- APP/MAP/map.c
+++ APP/MAP/map.c
@@ -4,9 +4,10 @@
 #include "time.h"
 
 const int MAP_SIZE = 7;
+const int MAP_TILE_SIZE = 5;
 const int MAX_BOSS_ROOM = 1;
 const int MAX_STARTING_ROOM = 1;
-const int MAC_SHOP_ROOM = 1;
+const int MAX_SHOP_ROOM = 1;
 const int MAX_ENNEMIES_ROOM = 20;
 
 void printMap(char** map) {
@@ -20,7 +21,13 @@ void printMap(char** map) {
 
 void fillMap(char** map) {
     srand(time(NULL));
-    int countBoss, countStarting, countShop, countEnnemies = 0;
+    int countStarting = 0;
+    int countBoss = 0;
+    int countShop = 0;
+    int countEnnemies = 0;
+    int sizeBoss = 0;
+    int sizeStarting = 0;
+    int sizeShop = 0;
 
     for (int i = 0; i < MAP_SIZE; i++) {
         for (int j = 0; j < MAP_SIZE; j++) {
@@ -30,27 +37,39 @@ void fillMap(char** map) {
                 int value = rand() % 6;
                 switch (value) {
                     case 0 :
-                        if (countBoss == MAX_BOSS_ROOM) {
+                        if (sizeBoss == (MAP_SIZE/2)+1) {
                             map[i][j] = 'B'; 
                             countBoss++;
+                            sizeBoss++;
+                        } else if (countBoss < MAX_BOSS_ROOM) {
+                            sizeBoss++;
+                            check++;
                         } else {
                             check++;
                         }
                         break;
 
                     case 1 :
-                        if (countStarting < MAX_STARTING_ROOM) {
+                        if (sizeStarting == (MAP_SIZE/2)+2) {
                             map[i][j] = 'X'; 
                             countStarting++;
+                            sizeStarting++;
+                        } else if (countStarting < MAX_STARTING_ROOM) {
+                            sizeStarting++;
+                            check++;
                         } else {
                             check++;
                         }
                         break;
                         
                     case 2 :
-                        if (countShop < MAC_SHOP_ROOM) {
+                        if (sizeShop == (MAP_SIZE/2)+3) {
                             map[i][j] = 'S'; 
                             countShop++;
+                            sizeShop++;
+                        } else if (countShop < MAX_SHOP_ROOM) {
+                            sizeShop++;
+                            check++;
                         } else {
                             check++;
                         }
@@ -77,6 +96,11 @@ void fillMap(char** map) {
             }
         }
     }
+
+    if (countBoss != 1 || countShop != 1 || countStarting != 1) {
+        printf("Ok\n");
+        fillMap(map);
+    }
 }
 
 void freeMap(char** map) {
@@ -85,6 +109,34 @@ void freeMap(char** map) {
     }
 
     free(map);
+}
+
+void printPlayableMap(char** map) {
+    int count = 0;
+    for (int i = 0; i < MAP_SIZE; count++) {
+        if (count % MAP_TILE_SIZE == 0 && count != 0) {
+            i++;
+        }
+        if (i == MAP_SIZE) {
+            break;
+        }
+        for (int j = 0; j < MAP_SIZE; j++) {
+            for (int k = 0; k < MAP_SIZE; k++) {
+                if (map[i][j] == 'B') {
+                    printf("\033[1;35m%c\033[0m", 'X');
+                } else if (map[i][j] == 'S') {
+                    printf("\033[1;32m%c\033[0m", 'X');
+                } else if (map[i][j] == 'R' || map[i][j] == 'X') {
+                    printf("X");
+                } else if (map[i][j] == 'V') {
+                    printf(" ");
+                } else if (map[i][j] == 'E') {
+                    printf("\033[1;31m%c\033[0m", 'X');
+                }
+            }
+        }
+        printf("\n");
+    }
 }
 
 void createMap() {
@@ -96,7 +148,9 @@ void createMap() {
 
     fillMap(map);
 
-    printMap(map);
+    //printMap(map);
+
+    printPlayableMap(map);
 
     freeMap(map);
 }
